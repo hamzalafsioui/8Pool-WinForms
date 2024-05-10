9951018d32fee3d57b87be0265fbde793c779374
hamzalafsioui <hamzalafsioui@gmail.com>
2024-05-10 11:00:35 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace _8Pool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void poolTable_OnTableComplete(object sender, PoolTable.TableCompletedEventArgs e)
+        {
+            string TableResults = "";
+
+            TableResults = "Time Consumed " + e.TimeText;
+            TableResults = TableResults + ", Total Seconds = " + e.TimeInSeconds;
+            TableResults = TableResults + ", HourlyRate = " + e.RatePerHour.ToString();
+            TableResults = TableResults + ", Total Fees = " + e.TotalFees.ToString();
+
+            MessageBox.Show(TableResults);
+        }
+    }
+}
